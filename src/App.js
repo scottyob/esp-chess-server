6ffb86c5f0af84c99e1a9a0a6ffb9f381124e681
Scott O'Brien <scott@scottyob.com>
2021-02-25 17:00:10 +0000

    Playing with setup scripts, and a different main pages and old school Twitter Bootstrap

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ from "react-router-dom";
 
 import * as queries from './graphql/queries';
 import { API } from 'aws-amplify';
+import { Home } from './components/Home.js';
 import React, { useEffect, useState } from 'react'
 
 import Amplify, { Auth } from 'aws-amplify';
@@ -23,7 +24,6 @@ Amplify.configure(awsconfig);
 
 function App() {
   return (<div className="App">
-    <header className="App-header">
       <Router>
         <Switch>
           <Route path="/setup/:ip">
@@ -35,30 +35,9 @@ function App() {
         </Switch>
       </Router>
     
-    </header>
   </div>);
 }
 
-
-
-const Home = () => {
-
-  const [msg, setMsg] = useState("");
-  useEffect(() => {
-    fetchMsg()
-  }, [])
-
-  async function fetchMsg() {
-    // const result = await API.graphql({query: queries.setup, variables: { msg: 'from client'}});
-
-    // const result = await API.graphql({query: queries.setup});
-    // debugger;
-    // // var data = JSON.parse(result.data.echo);
-    // setMsg(JSON.stringify(result.data));
-  }
-
-  return (<div><h2>Home sweet home - {msg}</h2></div>);
-}
 
 function Setup() {
   let { ip } = useParams();
